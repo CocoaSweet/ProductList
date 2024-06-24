cc13ed5b5ec392141a917d9421e258244363f6c0
CocoaSweet <111396916+CocoaSweet@users.noreply.github.com>
2024-06-24 16:11:46 -0300

    Update useProductData.ts

--- product-list/src/hooks/useProductData.ts
+++ product-list/src/hooks/useProductData.ts
@@ -2,7 +2,7 @@ import axios, {AxiosPromise} from "axios"
 import { ProductData } from "../interface/ProductData"
 import { useQuery } from "@tanstack/react-query"
 
-const API_URL = 'http://productlist-production-33aa.up.railway.app'
+const API_URL = 'http://localhost:8080'
 
 const fetchData = async (): AxiosPromise<ProductData[]> => {
     const response = axios.get(API_URL + '/product')
